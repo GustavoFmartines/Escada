153293926f34109791d6700c919134ebfa851edb
GustavoFmartines <gustavo.martines2003@outlook.com>
2025-04-04 20:14:26 -0300

    foi criado o programa que calcula o angulo de uma escada

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿double comprimentoEscada, Altura;
+double anguloGraus, anguloRadianus;
+
+Console.Clear();
+
+Console.ForegroundColor = ConsoleColor.DarkCyan;
+Console.BackgroundColor = ConsoleColor.Gray;
+
+Console.WriteLine("<---Escada--->\n\n");
+
+Console.ResetColor();
+
+Console.ForegroundColor = ConsoleColor.DarkCyan;
+
+
+Console.WriteLine("Informe o Comprimento da escada: ");
+comprimentoEscada = Convert.ToDouble(Console.ReadLine()!);
+
+Console.ResetColor();
+
+Console.ForegroundColor = ConsoleColor.DarkCyan;
+
+Console.WriteLine("Informe o ângulo (em graus) da posição da escada: ");
+anguloGraus = Convert.ToDouble(Console.ReadLine()!);
+
+Console.ResetColor();
+
+anguloRadianus = Math.PI * anguloGraus / 180;
+Altura = comprimentoEscada * Math.Sin(anguloRadianus);
+Altura = Math.Round(Altura, 2);
+
+Console.ForegroundColor = ConsoleColor.DarkCyan;
+
+Console.WriteLine($"Uma escada de {comprimentoEscada}m e um angulo de {anguloGraus}°, alcança {Altura}m na parede.");
+
+Console.ResetColor();
